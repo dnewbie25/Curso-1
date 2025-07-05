46ae4f48fb77db0422f1e134aade2a12c1c69d3f
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2025-07-05 16:28:22 -0500

    node http basic server created

--- 9.http.js
+++ 9.http.js
@@ -1,11 +1,21 @@
 const http = require('node:http')
+const { findAvailablePort } = require('./10.free-port')
+require('dotenv').config()
+
+const desiredPort = process.env.PORT ?? 3000 // 3000 is default
 
 const server = http.createServer((req, res) => {
   console.log('request received')
   res.end('Hola mundo!')
 })
 
-// el puerto 0 busca el primer puerto libre, no se recomienda en desarrollo
-server.listen(0, () => {
-  console.log(`http://localhost:${server.address().port}`)
+findAvailablePort(desiredPort).then(port => {
+  server.listen(port, () => {
+    console.log(`http://localhost:${port}`)
+  })
 })
+
+// // el puerto 0 busca el primer puerto libre, no se recomienda en desarrollo
+// server.listen(findAvailablePort(3000), () => {
+//   console.log(`http://localhost:${server.address().port}`)
+// })
